ae39bf436a0f246615ce206c1515269bc0bc826a
Quang <115056697+22T1020362@users.noreply.github.com>
2023-02-10 09:36:43 +0700

    Create Tong_Dua_Tren_So_Nguyen_To_Cung_Nhau.cpp

---
+++ Tong_Dua_Tren_So_Nguyen_To_Cung_Nhau.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using i64 = long long;
+
+
+void solve() {
+  i64 n , m;  cin >> n >> m;
+  cout << (n - 1) * (m - 1) / 2;
+}
+
+int main() {
+  cin.tie(nullptr) -> sync_with_stdio(false);
+  solve();
+}
